40c43ddc286d1e9b94024b226cd5cd12e62af823
Daniel Mason <danmasson0@gmail.com>
2022-07-19 13:26:30 -0400

    Updates

---
+++ precache-manifest.22ef79437a1eb7d2575b2bc6f78a8c5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cecfd5a9ecbfd8902afd4aac476f297c",
+    "url": "/color-generator-react-hook-practise/index.html"
+  },
+  {
+    "revision": "999131ddf18e89400a37",
+    "url": "/color-generator-react-hook-practise/static/css/main.9f3b6256.chunk.css"
+  },
+  {
+    "revision": "f600d8b467d65f2e88e8",
+    "url": "/color-generator-react-hook-practise/static/js/2.9014a315.chunk.js"
+  },
+  {
+    "revision": "85e50a0285581219fe721d1b9966173b",
+    "url": "/color-generator-react-hook-practise/static/js/2.9014a315.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "999131ddf18e89400a37",
+    "url": "/color-generator-react-hook-practise/static/js/main.91829694.chunk.js"
+  },
+  {
+    "revision": "325241ef9be83d779fc2",
+    "url": "/color-generator-react-hook-practise/static/js/runtime-main.213bec51.js"
+  }
+]);
