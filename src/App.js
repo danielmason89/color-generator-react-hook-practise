8e34cd58528d874ee7167bdfda706f25715c2180
Daniel Mason <danmasson0@gmail.com>
2022-07-19 11:05:24 -0400

    initial commit, set up codebase for color generator app, and implement submit functionality for app

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
+
+import Values from "values.js";
+
+function App() {
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>Color Generator App</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="type"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f105ad"
+            className={`${error ? "error" : null}`}
+          />
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        <h4>list goes here</h4>
+      </section>
+    </>
+  );
+}
+
+export default App;
